af4654083cf8e797953a316fe6d0c6964fa13301
Yassin <yassin.shehata@ucalgary.ca>
2025-09-23 20:15:49 -0600

    Update top_basys3_4bit.sv

--- top_basys3_4bit.sv
+++ top_basys3_4bit.sv
@@ -1,16 +1,15 @@
 // Basys3 wrapper for 4-bit ALU
 module top_basys3_4bit (
-  input  logic [15:0] sw,    // Basys3 switches
-  output logic [15:0] led    // Basys3 LEDs
+  input  logic [15:0] sw,  // Basys3 switches
+  output logic [15:0] led // Basys3 LEDs
 );
 
   // Map switches to ALU inputs
-  logic [3:0] a   = sw[3:0];     // A = SW0-SW3
-  logic [3:0] b   = sw[7:4];     // B = SW4-SW7
-  logic [3:0] op  = sw[11:8];    // opcode = SW8-SW11
+  logic [3:0] a   = sw[3:0] // A = SW0-SW3
+  logic [3:0] b   = sw[7:4];  // B = SW4-SW7
+  logic [3:0] op  = sw[11:8]; // opcode = SW8-SW11
+  logic [3:0] y;  // ALU result
 
-  // ALU result
-  logic [3:0] y;
 
   // Instantiate your ALU 
   alu dut (
@@ -20,11 +19,10 @@ module top_basys3_4bit (
     .result(y)
   );
 
-  // Drive LEDs:
  
-  assign led[15:12] = y;        // result
-  assign led[11:8]  = op;       // show opcode
-  assign led[7:4]   = b;        // show B
-  assign led[3:0]   = a;        // show A
+  assign led[15:12] = y;     // result
+  assign led[11:8]  = op;   // show opcode
+  assign led[7:4]   = b;     // show B
+  assign led[3:0]   = a;     // show A
 
 endmodule
